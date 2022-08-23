f25cb06f14b39916fd2594757268d964729fa70e
Edu Vieira <edu_vieira@hotmail.com>
2022-08-23 08:20:36 -0300

    Test templates

---
+++ src/test/java/example7/LoginUseCaseSyncTest1.java
@@ -0,0 +1,43 @@
+package example7;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LoginUseCaseSyncTest1 {
+
+    // region Constants
+
+    // endregion contants
+
+    // region helper fields
+
+    // endregion helperfields
+
+    LoginUseCaseSync SUT;
+
+    @Before
+    public void setup() throws Exception {
+        //SUT = new LoginUseCaseSync();
+
+    }
+
+    // helper methods
+
+    // endregion helper methods
+
+    // region helper classes
+
+    // endregion helper classes
+}
