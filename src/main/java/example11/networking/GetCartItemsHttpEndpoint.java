0b53e47fcf067521dcff034c3fae605346f35a8e
eduVieiraAF <edu_vieira@hotmail.com>
2022-09-05 14:35:22 -0300

    Setting up example 11 TDD

---
+++ src/main/java/example11/networking/GetCartItemsHttpEndpoint.java
@@ -0,0 +1,21 @@
+package example11.networking;
+
+import java.util.List;
+
+public interface GetCartItemsHttpEndpoint {
+    enum FailReason {
+        GENERAL_ERROR, NETWORK_ERROR
+    }
+
+    interface Callback {
+        void onGetCartItemsSucceeded(List<CartItemSchema> cartItems);
+        void onGetCartItemsFailed(FailReason failReason);
+    }
+
+    /**
+     * @param limit max amount of cart items to fetch
+     * @param callback object to be notified when the request completes
+     */
+
+    void getCartItems(int limit, Callback callback);
+}
